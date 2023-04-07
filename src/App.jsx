e31e24cce218a1347c58cfd8a93e32960f1f70c2
Teona <tekizashvili@gmail.com>
2023-04-07 11:09:49 +0400

    finished functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+
 function App() {
-  return <h1>hi</h1>;
+  const [markdown, setMarkdown] = useState('## markdown preview');
+
+  return (
+    <main>
+      <section className="markdown">
+        <textarea
+          className="input"
+          value={markdown}
+          onChange={(e) => setMarkdown(e.target.value)}
+        ></textarea>
+        <article className="result">
+          <ReactMarkdown>{markdown}</ReactMarkdown>
+        </article>
+      </section>
+    </main>
+  );
 }
 
 export default App;
